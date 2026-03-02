e42f099bdcde25438d971e8d069431f03b121308
Dmytro Odnoroh <falcon121299@icloud.com>
2026-03-02 17:13:13 +0200

    kateVR

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Вимикаємо вимогу 'use strict', бо в модулях він автоматичний
+    'strict': 'off',
+  },
 };
